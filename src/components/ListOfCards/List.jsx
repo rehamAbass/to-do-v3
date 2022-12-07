a3f1eb0eee987ca58c64a0d5163932f599fc2a94
Reham Abass <soft.rehamabass@gmail.com>
2022-12-07 20:53:23 +0200

    Update List.jsx

--- src/components/ListOfCards/List.jsx
+++ src/components/ListOfCards/List.jsx
@@ -3,19 +3,13 @@ import React from 'react'
 import Card from '../Card/Card'
 import '../../App.css'
 const GreenColors = [ 
-  'rgba(0, 190,0, 0.7)',
-  'rgba(0,10,100,0.7)',
-  'rgba(0,0,0,0.7)',
-  'rgba(95, 3, 95, 0.7)',
-        'rgba(0,40,125,0.7)',
-     'rgba(60,0,140, 0.7)', 
-  'rgba(180,180,0,0.7)', 
-
-    'rgba(30,0,60, 0.7)',
-  
-  'rgba(10,70,210,0.5)', 
-     'rgba(80,100,140,0.5)',  
-     'rgba(20,40,100,0.5)',
+  'rgba(0, 190,0, 0.6)', 
+  'rgba(10,70,210,0.6)', 
+     'rgba(80,100,140,0.6)',  
+     'rgba(20,40,100,0.6)',
+  'rgba(0,10,100,0.6)',
+  'rgba(180,180,0,0.6)', 
+    'rgba(30,0,60, 0.6)',
 ]
 
 let colors = GreenColors;
