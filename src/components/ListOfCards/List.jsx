26cd180181c21adf34694a55dce2479cdc945bfe
Reham Abass <soft.rehamabass@gmail.com>
2023-01-10 15:03:12 +0200

    Update List.jsx

--- src/components/ListOfCards/List.jsx
+++ src/components/ListOfCards/List.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 import Card from '../Card/Card'
 import '../../App.css'
 const GreenColors = [ 
+  '#C5DAE8','#A3D5FF', '#8D96C0', '#83C9F4','#828CBA',
+  
   //'#7EC8E3',
-  '#0000FF','#000C66',
-  'rgba(0,100,60,0.7)','rgba(0,70,155,0.7)','#050A30',
+ // '#0000FF','#000C66',
+//  'rgba(0,100,60,0.7)','rgba(0,70,155,0.7)','#050A30',
   //'#3dc7ff', // light blue
  // '#ffc759', //yellow
  // '#5265ff',  // blue 
